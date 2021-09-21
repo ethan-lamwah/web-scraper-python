889b5448a8363434516ef87f1cdf4a144b4423cd
Ethan Lam <hellowah5@gmail.com>
2021-09-21 23:49:00 +0800

    write the scraped result to file

--- scrape-every-minute.py
+++ scrape-every-minute.py
@@ -12,25 +12,37 @@ def find_jobs():
     soup = BeautifulSoup(page.content, "html.parser")
     job_list_section = soup.find("section", class_ ="job_list")
     job_list = job_list_section.find_all("div", class_="job")
-    for job in job_list:  
+    for index, job in enumerate(job_list):  
         tags = job["data-tags"]      
         if unfamiliar_skill not in tags:
-            title = job.h1.a.text.strip()
-            location = job.find("i", class_="i-globe").parent.text.strip()
-            company_name = job.find("i", class_="i-company").parent.text.strip()
-            post_date = job.find("i", class_="i-calendar").parent.text.strip()
-            contract_type = job.find("i", class_="i-chair").parent.text.strip()
-            detail = job.p.text.strip()
-            read_more = job.find("a", class_="go_button")["href"]
-            print("{:<16} {:<20}".format("Job Title:", title))
-            print("{:<16} {:<20}".format("Tags:", tags))
-            print("{:<16} {:<20}".format("Location:", location))
-            print("{:<16} {:<20}".format("Post by:", company_name))
-            print("{:<16} {:<20}".format("Post on:", post_date))
-            print("{:<16} {:<20}".format("Contract Type:", contract_type))
-            print("{:<16} {:<20}".format("Read more:", url + read_more[1:]))
-            print("{:<16} {:<20}".format("Detail:", detail))
-            print()
+            # *set encoding to solve 'illegal multibyte sequence'
+            with open(f'posts/{index}.txt', 'w+', encoding="utf-8") as f:
+                title = job.h1.a.text.strip()
+                location = job.find("i", class_="i-globe").parent.text.strip()
+                company_name = job.find("i", class_="i-company").parent.text.strip()
+                post_date = job.find("i", class_="i-calendar").parent.text.strip()
+                contract_type = job.find("i", class_="i-chair").parent.text.strip()
+                detail = job.p.text.strip()
+                read_more = job.find("a", class_="go_button")["href"]
+                f.write("{:<16} {:<20} \n".format("Job Title:", title))
+                f.write("{:<16} {:<20} \n".format("Tags:", tags))
+                f.write("{:<16} {:<20} \n".format("Location:", location))
+                f.write("{:<16} {:<20} \n".format("Post by:", company_name))
+                f.write("{:<16} {:<20} \n".format("Post on:", post_date))
+                f.write("{:<16} {:<20} \n".format("Contract Type:", contract_type))
+                f.write("{:<16} {:<20} \n".format("Read more:", url + read_more[1:]))
+                f.write("{:<16} {:<20} \n".format("Detail:", detail))
+            print(f'File saved: {index}')
+                # print("{:<16} {:<20}".format("Job Title:", title))
+                # print("{:<16} {:<20}".format("Tags:", tags))
+                # print("{:<16} {:<20}".format("Location:", location))
+                # print("{:<16} {:<20}".format("Post by:", company_name))
+                # print("{:<16} {:<20}".format("Post on:", post_date))
+                # print("{:<16} {:<20}".format("Contract Type:", contract_type))
+                # print("{:<16} {:<20}".format("Read more:", url + read_more[1:]))
+                # print("{:<16} {:<20}".format("Detail:", detail))
+                # print()
+            
 
     # print(f"{len(job_list)} Jobs Found.")
 
